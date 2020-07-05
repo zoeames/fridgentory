09f2951acc790e84dfc804d9820aacc14393cd88
Zoe Ames <zoeames@gmail.com>
2020-07-05 15:33:26 -0500

    added search

--- src/components/pages/Items/Items.js
+++ src/components/pages/Items/Items.js
@@ -11,11 +11,12 @@ import itemsData from '../../../helpers/data/itemsData';
 class Items extends React.Component {
   state = {
     items: [],
+    selectedItems: [],
   }
 
   getCompleteItemList = () => {
     smashData.getCompleteItemList()
-      .then((items) => this.setState({ items }))
+      .then((items) => this.setState({ items, selectedItems: items }))
       .catch((err) => console.error('unable to get items', err));
   }
 
@@ -29,14 +30,21 @@ class Items extends React.Component {
       .catch((err) => console.error('unable to delete item:', err));
   }
 
+  search = (e) => {
+    const selectedItems = this.state.items.filter((x) => x.name.toLowerCase().includes(e.target.value.toLowerCase()));
+    this.setState({ selectedItems });
+  }
+
   render() {
-    const { items } = this.state;
-    const buildItemCards = items.map((item) => <FoodCard key={item.id} item={item} deleteItem={this.deleteItem}/>);
+    const { selectedItems } = this.state;
+    const buildItemCards = selectedItems.map((item) => <FoodCard key={item.id} item={item} deleteItem={this.deleteItem}/>);
 
     return (
       <div className="Items col-12 text-center">
         <h1>Items</h1>
         <Link className="btn btn-dark" to='/items/new'><i className="fas fa-plus"></i></Link>
+        <br />
+        <input className="search" type="search" placeholder="Search" onChange={this.search} />
         <div className="d-flex flex-wrap">
           {buildItemCards}
         </div>
